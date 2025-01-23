5db60864cd70a64b778fb7a0208a844eaf6b0052
Md-SayeedAlam <muhammadsayeedalam9@gmail.com>
2025-01-23 21:37:08 +0600

    change code in  private route page

--- components/PrivateRoute.jsx
+++ components/PrivateRoute.jsx
@@ -1,13 +1,13 @@
-'use client'
-import Link from 'next/link';
-import { useRouter } from 'next/navigation';
+// 'use client'
+// import Link from 'next/link';
+// import { useRouter } from 'next/navigation';
 
 
 
-const PrivateRoute = () => {
-    const router = useRouter()
+// const PrivateRoute = () => {
+//     const router = useRouter()
     
-    return router.push('/api/auth/login')
+//     return router.push('/api/auth/login')
     
 
 
@@ -19,6 +19,24 @@ const PrivateRoute = () => {
         
     // </div>
     // );
+// };
+
+// export default PrivateRoute;
+
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+const PrivateRoute = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Redirect to the login page on component mount
+    router.push('/api/auth/login');
+  }, [router]);
+
+  return null; // Render nothing while redirecting
 };
 
 export default PrivateRoute;
