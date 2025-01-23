4c2e2679e0f68a2989e374f1861d728a94a49e01
Md-SayeedAlam <muhammadsayeedalam9@gmail.com>
2025-01-23 23:57:44 +0600

    added new files

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,8 @@
+
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -16,7 +19,5 @@ module.exports = {
   plugins: [
     require("daisyui"),
   ],
-  daisyui: {
-    themes: ["light","dark"],
-  },
+  
 };
