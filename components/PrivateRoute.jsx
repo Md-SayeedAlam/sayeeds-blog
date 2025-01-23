316acd8495e48d8ce8a905ff3e7b92324134143e
Md-SayeedAlam <muhammadsayeedalam9@gmail.com>
2025-01-23 23:29:47 +0600

    change some files

--- components/PrivateRoute.jsx
+++ components/PrivateRoute.jsx
@@ -1,42 +1,43 @@
-// 'use client'
-// import Link from 'next/link';
-// import { useRouter } from 'next/navigation';
+'use client'
+import Link from 'next/link';
+import { redirect, useRouter } from 'next/navigation';
 
 
 
-// const PrivateRoute = () => {
-//     const router = useRouter()
+const PrivateRoute = ({user}) => {
     
-//     return router.push('/api/auth/login')
+    const router = useRouter()
+    
+    if(!user) return  redirect('/api/auth/login')
     
 
 
 
          
-    // return (
-    // <div>
-    //     <h2>This is  private route . Please <Link href='/api/auth/login'><button className='text-sky-500'>Login</button></Link></h2>
+    return (
+    <div>
+        <h2>This is  private route . Please <Link href='/api/auth/login'><button className='text-sky-500'>Login</button></Link></h2>
         
-    // </div>
-    // );
-// };
-
-// export default PrivateRoute;
-
-'use client';
-
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-
-const PrivateRoute = () => {
-  const router = useRouter();
-
-  useEffect(() => {
-    // Redirect to the login page on component mount
-    router.push('/api/auth/login');
-  }, [router]);
-
-  return null; // Render nothing while redirecting
+    </div>
+    );
 };
 
 export default PrivateRoute;
+
+// 'use client';
+
+// import { useEffect } from 'react';
+// import { useRouter } from 'next/navigation';
+
+// const PrivateRoute = () => {
+//   const router = useRouter();
+
+//   useEffect(() => {
+    
+//     router.push('/api/auth/login');
+//   }, [router]);
+
+//   return null; 
+// };
+
+// export default PrivateRoute;
